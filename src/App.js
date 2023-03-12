fdba388515f6b57621514c986e27bd0a494d516d
Pawel Owiesek <owiesek.p@gmail.com>
2023-03-12 19:22:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="body js-body">
+      <button class="body__button js-imageButton">Change image</button>
+      <section class="calculator">
+        <h1 class="calculator__title">Currency calculator</h1>
+        <form class="calculator__form js-form">
+          <fieldset class="calculator__fieldset">
+            <p>
+              <label
+              ><span class="calculator__fieldset--label"
+              >Enter the amount of cash:</span
+                >
+                <input
+                  type="number"
+                  class="calculator__fieldset--field js-amount"
+                  step="any"
+                  min="1"
+                  required
+                  autofocus
+                /></label>
+            </p>
+            <p>
+              <label
+              ><span class="calculator__fieldset--label">Your currency:</span>
+                <select class="calculator__fieldset--field js-firstValue">
+                  <option value="Eur">Euro</option>
+                  <option value="Usd">U.S. Dollar</option>
+                  <option value="Gbp">British pound</option>
+                  <option value="Pln">Polish zloty</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label
+              ><span class="calculator__fieldset--label">Convert to:</span>
+                <select class="calculator__fieldset--field js-secondValue">
+                  <option value="Eur">Euro</option>
+                  <option value="Usd">U.S. Dollar</option>
+                  <option value="Gbp">British pound</option>
+                  <option value="Pln">Polish zloty</option>
+                </select>
+              </label>
+            </p>
+            <button class="calculator__form--button">Recalculate</button>
+            <p class="calculator__form--paragraph">
+              Exchange rate:<strong
+                class="calculator__form--rate js-exchangeRate"
+              ></strong>
+            </p>
+
+            <p class="calculator__form--paragraph">
+              Amount after conversion:<strong
+                class="calculator__form--rate js-result"
+              ></strong>
+            </p>
+          </fieldset>
+        </form>
+      </section>
+    </body>
   );
 }
 
