fa92bcc43dfea76d9c5835317b9a288617e99a69
Pawel Owiesek <owiesek.p@gmail.com>
2023-03-12 20:25:32 +0100

    component CurrentRate created with Css

---
+++ src/ExchangeRate/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+const CurrentRate = () => (
+    <p className="calculator__form--paragraph">
+        Exchange rate:<strong
+            className="calculator__form--rate"
+        ></strong>
+    </p>
+);
+
+export default CurrentRate;
