acccacfdd758c24c5f03c4786d13ba209c036e33
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-14 19:09:12 +0100

    created function to toggle background images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Button from "./Button";
 import Cash from "./Cash";
 import SelectClientCurrency from "./SelectClientCurrency";
@@ -7,13 +7,26 @@ import RecalculateButton from "./RecalculateButton";
 import ExchangeRate from "./ExchangeRate";
 import Result from "./Result";
 import Section from "./Section";
+import Container from "./Container";
 
 function App() {
 
+  const [image, setImage] = useState(`https://i.postimg.cc/wBrKx1wZ/images.jpg`);
+  const [showImage1, setShowImage1] = useState(true);
+
+  const toggleImage = () => {
+    if (showImage1) {
+      setImage(`https://i.postimg.cc/8z4DYzW4/giorgio-trovato-Wyxq-Qpy-FNk8-unsplash.jpg`)
+    } else {
+      setImage(`https://i.postimg.cc/wBrKx1wZ/images.jpg`)
+    }
+
+    setShowImage1(!showImage1);
+  }
   return (
 
-    <>
-      <Button
+    <Container image={image}>
+      <Button toggleImage={toggleImage}
       />
       <Section
         title="Currency calculator"
@@ -30,7 +43,9 @@ function App() {
             <Result />
           </>}
       />
-    </>
+    </Container>
+
+
   );
 }
 
