aadc65d4970a9083cb5c6896912e0ad758f283e3
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-14 19:11:24 +0100

    created Container component with background styles

---
+++ src/Container/index.js
@@ -0,0 +1,19 @@
+
+
+const Container = ({ image, children }) => {
+    const backgroundStyle = {
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        width: "100vw",
+    };
+
+    return (
+        <div className="backgroundImages" style={backgroundStyle}>
+            {children}
+        </div>
+    );
+};
+
+export default Container;
