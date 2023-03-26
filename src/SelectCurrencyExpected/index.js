c48a9394979a33209b3927527f6bc73652dd934d
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 18:53:16 +0200

    added interaction to select

--- src/SelectCurrencyExpected/index.js
+++ src/SelectCurrencyExpected/index.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
+import { currencyValues } from "../currencys"
 
 const SelectCarrencyExpected = () => {
-    const [convertCurrency, setConvertCurrency] = useState("Euro");
+    const [convertCurrency, setConvertCurrency] = useState(currencyValues[1].id);
     const onSelectChange = ({ target }) => setConvertCurrency(target.value);
     return (
         <p>
             <label
             ><span className="calculator__fieldset--label">Convert to:</span>
-                <select value={convertCurrency} onChange={onSelectChange} className="calculator__fieldset--field">
-                    <option value="Eur">Euro</option>
-                    <option value="Usd">U.S. Dollar</option>
-                    <option value="Gbp">British pound</option>
-                    <option value="Pln">Polish zloty</option>
+                <select
+                    value={convertCurrency}
+                    onChange={onSelectChange}
+                    className="calculator__fieldset--field">
+                    {currencyValues.map((convertCurrency => (<option
+                        value={convertCurrency.currencyName}
+                        key={convertCurrency.id}>
+                        {convertCurrency.currencyName}
+                    </option>
+                    )))}
                 </select>
             </label>
         </p>
