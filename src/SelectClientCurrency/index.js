bf8404d2187cf5df1662a221efd5f9e79f101f27
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 09:11:16 +0200

    added select interaction

--- src/SelectClientCurrency/index.js
+++ src/SelectClientCurrency/index.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
+
+const SelectClientCurrency = () => {
+    const [currency, setCurrency] = useState("");
+
+    const onSelectChange = ({ target }) => setCurrency(target.value);
+    return (
+        <p>
+            <label
+            ><span className="calculator__fieldset--label">Your currency:</span>
+                <select value={currency} onChange={onSelectChange} className="calculator__fieldset--field">
+                    <option value="Eur">Euro</option>
+                    <option value="Usd">U.S. Dollar</option>
+                    <option value="Gbp">British pound</option>
+                    <option value="Pln">Polish zloty</option>
+                </select>
+            </label>
+        </p>
+    )
+}
 
 
-const SelectClientCurrency = () => (
-    <p>
-        <label
-        ><span className="calculator__fieldset--label">Your currency:</span>
-            <select className="calculator__fieldset--field">
-                <option value="Eur">Euro</option>
-                <option value="Usd">U.S. Dollar</option>
-                <option value="Gbp">British pound</option>
-                <option value="Pln">Polish zloty</option>
-            </select>
-        </label>
-    </p>
-)
 
 export default SelectClientCurrency;
