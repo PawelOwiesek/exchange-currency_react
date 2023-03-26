77ec165af587554876d40172a61f729a95207d83
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 18:41:47 +0200

    imported currencyValues and interaction in select

--- src/SelectClientCurrency/index.js
+++ src/SelectClientCurrency/index.js
@@ -1,18 +1,26 @@
 import { useState } from "react";
+import { currencyValues } from "../currencys";
 
 const SelectClientCurrency = () => {
-    const [currency, setCurrency] = useState("Euro");
+    const [currency, setCurrency] = useState(currencyValues[0].id);
 
     const onSelectChange = ({ target }) => setCurrency(target.value);
     return (
         <p>
             <label>
                 <span className="calculator__fieldset--label">Your currency:</span>
-                <select value={currency} onChange={onSelectChange} className="calculator__fieldset--field">
-                    <option value="Eur">Euro</option>
-                    <option value="Usd">U.S. Dollar</option>
-                    <option value="Gbp">British pound</option>
-                    <option value="Pln">Polish zloty</option>
+                <select
+                    value={currency}
+                    onChange={onSelectChange}
+                    className="calculator__fieldset--field"
+                >
+                    {currencyValues.map((currency => (<option
+                        key={currency.id}
+                        value={currency.currencyName}
+                    >
+                        {currency.currencyName}
+                    </option>
+                    )))}
                 </select>
             </label>
         </p>
