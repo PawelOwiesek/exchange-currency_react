17c3d685f67f8f80b336a09cef990d973407d054
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 09:13:26 +0200

    SelectClientCurrency formated

--- src/SelectClientCurrency/index.js
+++ src/SelectClientCurrency/index.js
@@ -6,8 +6,8 @@ const SelectClientCurrency = () => {
     const onSelectChange = ({ target }) => setCurrency(target.value);
     return (
         <p>
-            <label
-            ><span className="calculator__fieldset--label">Your currency:</span>
+            <label>
+                <span className="calculator__fieldset--label">Your currency:</span>
                 <select value={currency} onChange={onSelectChange} className="calculator__fieldset--field">
                     <option value="Eur">Euro</option>
                     <option value="Usd">U.S. Dollar</option>
