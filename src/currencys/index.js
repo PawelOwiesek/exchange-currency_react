8ccf4ab4117d6c5d93af7e52f5b66dc2f06fde9c
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 12:42:43 +0200

    created module with currencyValues

---
+++ src/currencys/index.js
@@ -0,0 +1,27 @@
+
+export const currencyValues = [
+    {
+        currencyName: "Euro",
+        short: "Eur",
+        value: 4.692,
+        id: 1
+    },
+    {
+        currencyName: "Dolar amerykanski",
+        short: "Usd",
+        value: 4.2928,
+        id: 2
+    },
+    {
+        currencyName: "Britain pound",
+        short: "Gbp",
+        value: 5.2571,
+        id: 3
+    },
+    {
+        currencyName: "Polish zloty",
+        short: "Pln",
+        value: 1,
+        id: 4
+    },
+];
