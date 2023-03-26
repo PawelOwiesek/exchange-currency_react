f07b41d532e4c245729a9ca8f8a502cf9839bc8a
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 09:26:14 +0200

    created onSelectChange function & defaul value

--- src/SelectCurrencyExpected/index.js
+++ src/SelectCurrencyExpected/index.js
@@ -1,16 +1,21 @@
+import { useState } from "react";
 
-const SelectCarrencyExpected = () => (
-    <p>
-        <label
-        ><span className="calculator__fieldset--label">Convert to:</span>
-            <select className="calculator__fieldset--field">
-                <option value="Eur">Euro</option>
-                <option value="Usd">U.S. Dollar</option>
-                <option value="Gbp">British pound</option>
-                <option value="Pln">Polish zloty</option>
-            </select>
-        </label>
-    </p>
-);
+const SelectCarrencyExpected = () => {
+    const [convertCurrency, setConvertCurrency] = useState("Euro");
+    const onSelectChange = ({ target }) => setConvertCurrency(target.value);
+    return (
+        <p>
+            <label
+            ><span className="calculator__fieldset--label">Convert to:</span>
+                <select value={convertCurrency} onChange={onSelectChange} className="calculator__fieldset--field">
+                    <option value="Eur">Euro</option>
+                    <option value="Usd">U.S. Dollar</option>
+                    <option value="Gbp">British pound</option>
+                    <option value="Pln">Polish zloty</option>
+                </select>
+            </label>
+        </p>
+    );
 
+}
 export default SelectCarrencyExpected;
