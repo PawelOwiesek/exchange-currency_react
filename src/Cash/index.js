c2b1f79d08e1ab84c8d78f6aaa70de33eaaaf454
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 09:09:18 +0200

    added interaction to Cash component input

--- src/Cash/index.js
+++ src/Cash/index.js
@@ -1,19 +1,30 @@
 import "./index.css";
+import { useState } from "react";
 
-const Cash = () => (
-    <p>
-        <label
-        ><span className="calculator__fieldset--label"
-        >Enter the amount of cash:</span
-            >
-            <input
-                type="number"
-                className="calculator__fieldset--field"
-                step="any"
-                min="1"
-                required
-            /></label>
-    </p>
-);
+const Cash = () => {
+    const [cash, setCash] = useState("");
+
+    return (
+        <p>
+            <label
+            ><span className="calculator__fieldset--label"
+            >Enter the amount of cash:</span
+                >
+                <input
+                    value={cash}
+                    onChange={({ target }) => setCash(target.value)}
+                    type="number"
+                    className="calculator__fieldset--field"
+                    step="any"
+                    min="1"
+                    required
+                />
+            </label>
+        </p>
+    );
+
+};
+
+
 
 export default Cash;
