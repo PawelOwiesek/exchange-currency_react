365d34963e29a1f9d82a525f4828bbb413e0ea2d
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-26 09:10:19 +0200

    added preventDefault on form

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,14 +1,21 @@
 import "./index.css";
 
-const Section = ({ title, body, result }) => (
-    <section className="calculator">
-        <h1 className="calculator__title">{title}</h1>
-        <form className="calculator__form">
-            <fieldset className="calculator__fieldset">
-                {body}
-                {result}
-            </fieldset>
-        </form>
-    </section>
-)
+const Section = ({ title, body, result }) => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <section className="calculator">
+            <h1 className="calculator__title">{title}</h1>
+            <form onSubmit={onFormSubmit} className="calculator__form">
+                <fieldset className="calculator__fieldset">
+                    {body}
+                    {result}
+                </fieldset>
+            </form>
+        </section>
+    )
+}
+
+
 export default Section;
