dd51e5e3a774312ce13554c05dc50feaa37a64c1
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-30 16:04:31 +0200

    created function calculateResult onClick

--- src/RecalculateButton/index.js
+++ src/RecalculateButton/index.js
@@ -1,7 +1,21 @@
 import "./index.css";
 
-const RecalculateButton = () => (
-    <button className="calculator__form--button">Recalculate</button>
-);
+const RecalculateButton = ({ cashValue, currency, convertCurrency }) => {
+    const calculateResult = () => {
+        const cashInput = +cashValue;
+        const currencySelect = parseFloat(currency);
+        const convertCurrencySelect = parseFloat(convertCurrency);
+        const result = ((cashInput * currencySelect) / convertCurrencySelect);
+        console.log(cashInput);
+        console.log(currency);
+        console.log(convertCurrency);
+        console.log(result);
+    }
+
+    return (
+        <button onClick={calculateResult} className="calculator__form--button">Recalculate</button>
+    );
+
+}
 
 export default RecalculateButton;
