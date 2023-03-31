f10cf4e91212b1dca759cd01b4920437c8fbaa43
Pawel Owiesek <Owiesek.p@gmail.com>
2023-03-31 15:16:28 +0200

    render of component with result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,9 +1,11 @@
-const Result = () => {
-    <p className="calculator__form--paragraph">
-        Amount after conversion:<strong
-            className="calculator__form--rate"
-        ></strong>
-    </p>
-}
+const Result = ({ result }) => {
+    return (
+        <p className="calculator__form--paragraph">
+            Amount after conversion:<strong
+                className="calculator__form--rate"
+            >{result}</strong>
+        </p>
+    );
+};
 
 export default Result;
