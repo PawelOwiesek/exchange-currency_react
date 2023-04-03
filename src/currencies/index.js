4a1d6acbff2d92acdfd56e49a4a62a03186d9636
Pawel Owiesek <Owiesek.p@gmail.com>
2023-04-03 10:32:56 +0200

    removed short names

--- src/currencies/index.js
+++ src/currencies/index.js
@@ -1,26 +1,22 @@
 export const currencyValues = [
     {
         currencyName: "Euro",
-        short: "Eur",
         value: 4.692,
         id: 1
     },
     {
         currencyName: "US dolar ",
-        short: "Usd",
         value: 4.2928,
         id: 2
     },
     {
         currencyName: "Britain pound",
-        short: "Gbp",
         value: 5.2571,
         id: 3
     },
     {
         currencyName: "Polish zloty",
-        short: "Pln",
-        value: 1,
+        value: 1.00,
         id: 4
     },
 ];
