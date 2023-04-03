62f7b20e4813e5986010873e748464bfe2fa0d13
Pawel Owiesek <Owiesek.p@gmail.com>
2023-04-03 08:04:46 +0200

    renamed class on button

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 
 const Button = ({ toggleImage }) => (
-    <button onClick={toggleImage} className="body__button">Change image</button>)
+    <button onClick={toggleImage} className="button">Change image</button>)
 
 export default Button;
