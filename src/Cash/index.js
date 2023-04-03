7cea3569e34a18402636cebe2bef08db951d7b60
Pawel Owiesek <Owiesek.p@gmail.com>
2023-04-03 08:06:26 +0200

    removed return ffrom component

--- src/Cash/index.js
+++ src/Cash/index.js
@@ -1,25 +1,24 @@
 import "./index.css";
 
-const Cash = ({ cashValue, handleCashChange }) => {
-    return (
-        <p>
-            <label
-            ><span className="calculator__fieldset--label"
-            >Enter the amount of cash:</span
-                >
-                <input
-                    value={cashValue}
-                    onChange={handleCashChange}
-                    type="number"
-                    className="calculator__fieldset--field"
-                    step="any"
-                    min="1"
-                    required
-                />
-            </label>
-        </p>
-    );
-};
+const Cash = ({ cashValue, handleCashChange }) => (
+    <p>
+        <label
+        ><span className="calculator__fieldset--label"
+        >Enter the amount of cash:</span
+            >
+            <input
+                value={cashValue}
+                onChange={handleCashChange}
+                type="number"
+                className="calculator__fieldset--field"
+                step="any"
+                min="1"
+                required
+            />
+        </label>
+    </p>
+);
+
 
 
 
