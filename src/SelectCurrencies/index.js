91bf4d858d3d1a5ce270cf1006be2111f44705ad
Pawel Owiesek <Owiesek.p@gmail.com>
2023-04-11 14:09:45 +0200

    changed option value convertCurrency.currencyName

--- src/SelectCurrencies/index.js
+++ src/SelectCurrencies/index.js
@@ -30,7 +30,7 @@ const SelectCurrencies = ({ currency, onSelectChange, convertCurrency, onSecondS
                         onChange={onSecondSelectChange}
                         className="calculator__fieldset--field">
                         {currencyValues.map((convertCurrency => (<option
-                            value={convertCurrency.value}
+                            value={convertCurrency.currencyName}
                             key={convertCurrency.id}>
                             {convertCurrency.currencyName}
                         </option>
