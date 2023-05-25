0bb246e560e7316975286bc9819313b64b3708d0
Pawel Owiesek <Owiesek.p@gmail.com>
2023-05-25 11:02:11 +0200

    moved styled component to FetchApi

--- src/SectionForm/styled.js
+++ src/SectionForm/styled.js
@@ -32,23 +32,3 @@ export const StyledFieldset = styled.fieldset`
     flex-wrap: wrap;
   }
 `;
-
-export const Loading = styled.div`
-  width: fit-content;
-  height: fit-content;
-  display: flex;
-  align-self: center;
-  justify-content: center;
-  align-items: center;
-  font-size: 15px;
-  letter-spacing: 1.5px;
-  padding: 20px;
-  margin-bottom: 10px;
-  color: ${({ theme }) => theme.colors.white};
-  background-color: ${({ theme }) => theme.colors.transparentBlue};
-  border-radius: 25px;
-  border: 2px solid ${({ theme }) => theme.colors.white};
-`;
-export const Error = styled(Loading)`
-  background-color: ${({ theme }) => theme.colors.crimson};
-`;
