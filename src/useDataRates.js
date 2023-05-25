6bf808982448fb45eed682a7c0f83a43ee5afdec
Pawel Owiesek <Owiesek.p@gmail.com>
2023-05-25 09:42:23 +0200

    Assigning the server address to a variable.

--- src/useDataRates.js
+++ src/useDataRates.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const fetchAPI = "https://api.exchangerate.host/latest?base=PLN";
+
 export const useDataRates = () => {
   const [data, setData] = useState({ state: "loading" });
 
   useEffect(() => {
     const fetchRates = async () => {
       try {
-        const response = await axios.get(
-          "https://api.exchangerate.host/latest?base=PLN"
-        );
+        const response = await axios.get(fetchAPI);
         setData({
           state: "success",
           rates: response.data.rates,
